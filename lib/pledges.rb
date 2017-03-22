e47a67595aca73ccf138749eeb9a58ce5b054510
Rafael dos Santos Silva <xfalcox@gmail.com>
2017-03-22 19:22:29 -0300

    FIX: More fixes to empty reward tiers

--- lib/pledges.rb
+++ lib/pledges.rb
@@ -110,7 +110,7 @@ module ::Patreon
     def self.find_user_by_rewards rewards
       reward_users = ::PluginStore.get(PLUGIN_NAME, 'reward-users')
 
-      rewards.map {|id| reward_users[id] }.reduce(:+).uniq
+      rewards.map {|id| reward_users[id] }.compact.flatten.uniq
     end
 
     def self.patreon_users_to_discourse_users patreon_users_ids
