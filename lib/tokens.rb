0377d19f7e8c38e512e62fa0bb11bd8d4b74413f
Rafael dos Santos Silva <xfalcox@gmail.com>
2017-04-25 23:53:33 -0300

    Make the plugin work without any extra gems

--- lib/tokens.rb
+++ lib/tokens.rb
@@ -1,12 +1,20 @@
-require 'patreon'
+require 'json'
 
 module ::Patreon
   class Tokens
     def self.update!
       redirect_uri = "#{Discourse.base_url}/auth/patreon/callback"
 
-      oauth_client = Patreon::OAuth.new(SiteSetting.patreon_client_id, SiteSetting.patreon_client_secret)
-      tokens = oauth_client.refresh_token(SiteSetting.patreon_creator_refresh_token, redirect_uri)
+      conn = Faraday.new(url: 'https://api.patreon.com')
+
+      response = conn.post('/oauth2/token', {
+          grant_type: 'refresh_token',
+          refresh_token: SiteSetting.patreon_creator_refresh_token,
+          client_id: SiteSetting.patreon_client_id,
+          client_secret: SiteSetting.patreon_client_secret
+        })
+
+      tokens = JSON.parse response.body
 
       SiteSetting.patreon_creator_access_token = tokens['access_token']
       SiteSetting.patreon_creator_refresh_token = tokens['refresh_token']
