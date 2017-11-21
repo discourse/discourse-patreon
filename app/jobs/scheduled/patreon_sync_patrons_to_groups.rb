f24a57a0a164b9fa6bd53bc1b887744615ff3123
Guo Xiang Tan <tgx_world@hotmail.com>
2017-11-21 09:06:27 +0800

    Don't retry if scheduled job fails.

--- app/jobs/scheduled/patreon_sync_patrons_to_groups.rb
+++ app/jobs/scheduled/patreon_sync_patrons_to_groups.rb
@@ -1,6 +1,7 @@
 module ::Jobs
   class PatreonSyncPatronsToGroups < ::Jobs::Scheduled
     every 6.hours
+    sidekiq_options retry: false
 
     def execute(args)
       ::Patreon::Pledges.update_patrons! if SiteSetting.patreon_enabled && SiteSetting.patreon_creator_access_token && SiteSetting.patreon_creator_refresh_token
