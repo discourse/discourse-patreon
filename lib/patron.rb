e55d433eb1108fa804c20a1ef7ea15342dd0f52a
Vinoth Kannan <vinothkannan@vinkas.com>
2019-10-08 23:25:10 +0530

    PERF: Reduce number of queries sent to the DB.

--- lib/patron.rb
+++ lib/patron.rb
@@ -18,30 +18,32 @@ module ::Patreon
       return if filters.blank?
 
       local_users = get_local_users
+      reward_users = Patreon::RewardUser.all
+      declined_users = Patreon::Pledge::Decline.all
+      declined_pledges_grace_period_days = SiteSetting.patreon_declined_pledges_grace_period_days
 
       filters.each_pair do |group_id, rewards|
         group = Group.find_by(id: group_id)
 
         next if group.nil?
 
-        reward_users = Patreon::RewardUser.all
         patron_ids = rewards.map { |id| reward_users[id] }.compact.flatten.uniq
 
         next if patron_ids.blank?
 
         user_ids = local_users.select do |_, patreon_id|
           is_declined = false
-          declined_since = Patreon::Pledge::Decline.all[patreon_id]
+          declined_since = declined_users[patreon_id]
 
           if declined_since.present?
             declined_days_count = Time.now.to_date - declined_since.to_date
-            is_declined = declined_days_count > SiteSetting.patreon_declined_pledges_grace_period_days
+            is_declined = declined_days_count > declined_pledges_grace_period_days
           end
 
           patreon_id.present? && patron_ids.include?(patreon_id) && !is_declined
         end.pluck(0)
 
-        group_user_ids = group.users.pluck(:id)
+        group_user_ids = GroupUser.where(group: group).pluck(:user_id)
 
         User.where(id: (user_ids - group_user_ids)).each do |user|
           group.add user
