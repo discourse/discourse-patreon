256743418af27ad97e658237b73a54024ad9263f
David Taylor <david@taylorhq.com>
2021-04-26 10:15:04 +0100

    FIX: Never add query params to the callback URL (#52)
    We never want query params to be added to the callback URL. This same override is used in a number of OAuth2 omniauth providers, including GitHub and Google.

--- plugin.rb
+++ plugin.rb
@@ -154,6 +154,10 @@ after_initialize do
         response.parsed
       end
     end
+
+    def callback_url
+      full_host + script_name + callback_path
+    end
   end
 
   DiscourseEvent.on(:user_created) do |user|
