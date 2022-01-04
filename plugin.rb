discourse/discourse-patreon

commit 66aa0d00eddfcf0561be8015d4ad756ccff5ad6e

DEV: Migrate to ManagedAuthenticator and UserAssociatedAccount
Data will be automatically migrated from the `oauth2_user_infos` table to the `user_associated_accounts` table

plugin.rb
@@ -6,7 +6,6 @@
 # author: Rafael dos Santos Silva <xfalcox@gmail.com>
 # url: https://github.com/discourse/discourse-patreon
 
-require 'auth/oauth2_authenticator'
 require 'omniauth-oauth2'
 
 enabled_site_setting :patreon_enabled
@@ -178,9 +177,7 @@ class ::OmniAuth::Strategies::Patreon < ::OmniAuth::Strategies::OAuth2
   info do
     {
       email: raw_info['data']['attributes']['email'],
-      name: raw_info['data']['attributes']['full_name'],
-      access_token: access_token.token,
-      refresh_token: access_token.refresh_token
+      name: raw_info['data']['attributes']['full_name']
     }
   end
 
@@ -204,7 +201,7 @@ class ::OmniAuth::Strategies::Patreon < ::OmniAuth::Strategies::OAuth2
   end
 end
 
-class Auth::PatreonAuthenticator < Auth::OAuth2Authenticator
+class Auth::PatreonAuthenticator < Auth::ManagedAuthenticator
   def register_middleware(omniauth)
     omniauth.provider :patreon,
                       setup: lambda { |env|
@@ -216,14 +213,14 @@ class Auth::PatreonAuthenticator < Auth::OAuth2Authenticator
                       }
   end
 
-  def after_authenticate(auth_token)
+  def after_authenticate(auth_token, existing_account: nil)
     result = super
 
     user = result.user
     discourse_username = SiteSetting.patreon_creator_discourse_username
     if discourse_username.present? && user && user.username == discourse_username
-      SiteSetting.patreon_creator_access_token = auth_token[:info][:access_token]
-      SiteSetting.patreon_creator_refresh_token = auth_token[:info][:refresh_token]
+      SiteSetting.patreon_creator_access_token = auth_token.credentials["access_token"]
+      SiteSetting.patreon_creator_refresh_token = auth_token.credentials["refresh_token"]
     end
 
     result
@@ -234,4 +231,4 @@ class Auth::PatreonAuthenticator < Auth::OAuth2Authenticator
   end
 end
 
-auth_provider authenticator: Auth::PatreonAuthenticator.new('patreon', trusted: true)
+auth_provider authenticator: Auth::PatreonAuthenticator.new

db/migrate/20211230151700_migrate_patreon_user_info.rb
@@ -1,0 +1,31 @@
+# frozen_string_literal: true
+
+class MigratePatreonUserInfo < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+    INSERT INTO user_associated_accounts (
+      provider_name,
+      provider_uid,
+      user_id,
+      info,
+      last_used,
+      created_at,
+      updated_at
+    ) SELECT
+      'patreon',
+      uid,
+      user_id,
+      json_build_object('email', email, 'name', name),
+      updated_at,
+      created_at,
+      updated_at
+    FROM oauth2_user_infos
+    WHERE provider = 'patreon'
+    ON CONFLICT DO NOTHING
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end

lib/patron.rb
@@ -129,13 +129,13 @@ module ::Patreon
       users = User.joins("INNER JOIN user_custom_fields cf ON cf.user_id = users.id AND cf.name = 'patreon_id'").pluck("users.id, cf.value")
       patrons = all.slice!(*users.pluck(1))
 
-      oauth_users = Oauth2UserInfo.includes(:user).where(provider: "patreon")
-      oauth_users = oauth_users.where("uid IN (?)", patrons.keys) if patrons.present?
+      oauth_users = UserAssociatedAccount.includes(:user).where(provider_name: "patreon")
+      oauth_users = oauth_users.where("provider_uid IN (?)", patrons.keys) if patrons.present?
 
       users += oauth_users.map do |o|
-        patrons = patrons.slice!(o.uid)
-        update_local_user(o.user, o.uid)
-        [o.user_id, o.uid]
+        patrons = patrons.slice!(o.provider_uid)
+        update_local_user(o.user, o.provider_uid)
+        [o.user_id, o.provider_uid]
       end
 
       emails = patrons.values.map { |e| e.downcase }
@@ -152,7 +152,7 @@ module ::Patreon
       user = User.joins(:_custom_fields).find_by(user_custom_fields: { name: 'patreon_id', value: patreon_id })
       return user if user.present?
 
-      user = User.joins(:oauth2_user_infos).find_by(oauth2_user_infos: { provider: "patreon", uid: patreon_id })
+      user = User.joins(:user_associated_accounts).find_by(user_associated_accounts: { provider_name: "patreon", provider_uid: patreon_id })
       user ||= User.joins(:user_emails).find_by(user_emails: { email: all[patreon_id] })
       return if user.blank?
 

spec/patron_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe ::Patreon::Patron do
 
-  Fabricator(:oauth2_user_info) do
-    provider "patreon"
+  Fabricator(:patreon_user_info, class_name: :user_associated_account) do
+    provider_name "patreon"
     user
   end
 
@@ -24,7 +24,7 @@ RSpec.describe ::Patreon::Patron do
 
   it "should find local users matching Patreon user info" do
     Fabricate(:user, email: "foo@bar.com")
-    Fabricate(:oauth2_user_info, uid: "111112")
+    Fabricate(:patreon_user_info, provider_uid: "111112")
 
     local_users = described_class.get_local_users
     expect(local_users.count).to eq(2)
@@ -47,7 +47,7 @@ RSpec.describe ::Patreon::Patron do
   end
 
   describe "sync groups" do
-    let(:ouser) { Fabricate(:oauth2_user_info, uid: "111112") }
+    let(:ouser) { Fabricate(:patreon_user_info, provider_uid: "111112") }
     let(:group1) { Fabricate(:group) }
     let(:group2) { Fabricate(:group) }
 
@@ -64,7 +64,7 @@ RSpec.describe ::Patreon::Patron do
     end
 
     it "should sync by Patreon id" do
-      described_class.sync_groups_by(patreon_id: ouser.uid)
+      described_class.sync_groups_by(patreon_id: ouser.provider_uid)
       expect(group1.users.to_a).to eq([ouser.user])
       expect(group2.users.to_a).to eq([ouser.user])
     end

commit 22ad39ae8fb976c5ba6c05d9324b473a4a407d62

FIX: Define name in PatreonAuthenticator
This regressed in 66aa0d00

plugin.rb
@@ -202,6 +202,10 @@ class ::OmniAuth::Strategies::Patreon < ::OmniAuth::Strategies::OAuth2
 end
 
 class Auth::PatreonAuthenticator < Auth::ManagedAuthenticator
+  def name
+    "patreon"
+  end
+
   def register_middleware(omniauth)
     omniauth.provider :patreon,
                       setup: lambda { |env|