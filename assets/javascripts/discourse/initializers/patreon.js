6a07c3c2e545c7d259090df300c857096fdc1979
David Taylor <david@taylorhq.com>
2023-11-23 16:27:07 +0000

    DEV: Resolve `registerConnectorClass` deprecation

--- assets/javascripts/discourse/initializers/patreon.js
+++ assets/javascripts/discourse/initializers/patreon.js
@@ -24,37 +24,6 @@ function initWithApi(api) {
       numTopicsOpened++;
     }
   });
-
-  api.registerConnectorClass("topic-above-footer-buttons", "patreon", {
-    shouldRender(_args, component) {
-      return component.currentUser;
-    },
-
-    setupComponent(_args, component) {
-      component.didInsertElement = function () {
-        const showDonationPrompt =
-          this.siteSettings.patreon_enabled &&
-          this.siteSettings.patreon_donation_prompt_enabled &&
-          this.siteSettings.patreon_donation_prompt_campaign_url !== "" &&
-          this.currentUser.show_donation_prompt &&
-          $.cookie(cookieName) !== "t" &&
-          numTopicsOpened >
-            this.siteSettings.patreon_donation_prompt_show_after_topics;
-
-        this.set("showDonationPrompt", showDonationPrompt);
-      };
-    },
-
-    actions: {
-      close() {
-        // hide the donation prompt for 30 days
-        const expires = moment().add(30, "d").toDate();
-        $.cookie(cookieName, "t", { expires });
-
-        $(this.element).fadeOut(700);
-      },
-    },
-  });
 }
 
 export default {
